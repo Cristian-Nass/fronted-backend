5ab4705b8f8dd89b1d9727e33b4dd3be83d89b3b
Cristian Nass <cristian.tirdad@gmail.com>
2023-10-11 20:46:23 +0200

    get specific data with id

--- routes/posts.js
+++ routes/posts.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("Post");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-// router.get("/specific", (req, res) => {
-//   res.send("Specific Post");
-// });
 router.post("/", async (req, res) => {
   console.log(req.body);
   const post = new Post({
@@ -18,6 +20,15 @@ router.post("/", async (req, res) => {
   try {
     const savePost = await post.save();
     res.json(savePost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
   } catch (err) {
     res.json({ message: err });
   }
